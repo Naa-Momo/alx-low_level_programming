ffe941a0736d994e03a486fc5771bdf9c3ea0d0e
Valerie Afotey Williams <Valerie Afotey Williams>
2023-08-24 09:58:18 -0700

    0x12-singly_linked_lists

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,18 +1,20 @@
+#include <stdlib.h>
 #include "lists.h"
 
 /**
- * list_len - lenght of the list
- * @h: points to node
- * Return: number of nodes
+ * list_len - returns the number of elements in a linked list
+ * @h: pointer to the list_t list
+ *
+ * Return: number of elements in h
  */
 size_t list_len(const list_t *h)
 {
-	unsigned int i = 0;
+	size_t n = 0;
 
-	while (h != NULL)
+	while (h)
 	{
-		i++;
+		n++;
 		h = h->next;
 	}
-	return (i);
+	return (n);
 }
